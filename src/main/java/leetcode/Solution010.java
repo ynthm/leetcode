c99a3b7e8e8488e9152ca0e1ea134134aedc2d2f
Ynthm Wang <ynthm.w@gmail.com>
2018-07-23 13:49:06 +0800

    update .gitignore

--- src/main/java/leetcode/Solution010.java
+++ src/main/java/leetcode/Solution010.java
@@ -1,6 +1,17 @@
 package leetcode;
 
 public class Solution010 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
     public int[] twoSum(int[] nums, int target) {
         return null;
     }
--- src/test/java/leetcode/TestSolution010.java
+++ src/test/java/leetcode/TestSolution010.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestSolution010 {
+
     @Test
     @DisplayName("Two Sum")
     void testTwoNum()
